97cf9be86b43cc6a2ffbcd35d8c36af409dc44c1
Aman <aman@Admins-MacBook-Air.local>
2025-04-14 11:05:10 +0530

    Added Appointment Model and Controller

--- Backend/src/controllers/doctor.controller.js
+++ Backend/src/controllers/doctor.controller.js
@@ -1,4 +1,6 @@
+import appointmentModel from "../models/appointment.model.js";
 import doctorModel from "../models/doctor.model.js";
+import userModel from "../models/user.model.js";
 
 const changeAvailablity = async (req, res) => {
   try {
@@ -17,8 +19,62 @@ const changeAvailablity = async (req, res) => {
   }
 };
 
-const editDoctor = async (req,res)=>{
-  
-}
+const bookDocAppointment = async (req, res) => {
+  try {
+    const { userId, docId, slotDate, slotTime } = req.body;
 
-export { changeAvailablity };
+    const docData = await doctorModel.findById(docId).select("-password");
+
+    if (!docData.available) {
+      return res.json({ success: false, message: "Doctor is not Available" });
+    }
+    let slots_booked = docData.slots_booked;
+
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({ success: false, message: "Slot is not Available" });
+      } else {
+        slots_booked[slotDate].push(slotTime);
+      }
+    } else {
+      slots_booked[slotDate] = [];
+      slots_booked[slotDate].push(slotTime);
+    }
+
+    delete docData.slots_booked;
+
+    const userData = await userModel.findById(userId).select("-password");
+
+    const appointmentData = {
+      userId,
+      docId,
+      slotDate,
+      slotTime,
+      userData,
+      docData,
+      amount: docData.fees,
+      date: Date.now(),
+    };
+
+    const newAppointment = new appointmentModel(appointmentData);
+
+    await newAppointment.save();
+
+    await doctorModel.findByIdAndUpdate(docId,{slots_booked});
+
+    return res.json({
+      success: true,
+      message: "Appointment Booked Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const editDoctor = async (req, res) => {};
+
+export { changeAvailablity,bookDocAppointment };
